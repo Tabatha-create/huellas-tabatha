d9c9e071f7a3e904c60871a2263342b70b6a6b5a
Tabatha-create <huellasdetabatha@gmail.com>
2026-01-04 20:05:46 +0100

    3ra prueba

--- app-huellas-tabatha/js/script.js
+++ app-huellas-tabatha/js/script.js
@@ -1,6 +1,8 @@
  // Registramos el service worker para PWA
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(() => {
-    console.log("Service Worker registrado para la PWA de cuentos.");
-  });
+  navigator.serviceWorker
+    .register("./sw.js", { scope: "./" })
+    .then(() => console.log("SW registrado correctamente"))
+    .catch(err => console.error("SW error:", err));
 }
+
