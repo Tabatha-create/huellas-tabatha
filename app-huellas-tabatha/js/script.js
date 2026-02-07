0fdda65761e3496d8f5394194ab4ab25889aa365
Tabatha-create <huellasdetabatha@gmail.com>
2026-02-07 19:36:42 +0100

    Cambios 07-Feb-2026

--- app-huellas-tabatha/js/script.js
+++ app-huellas-tabatha/js/script.js
@@ -1,7 +1,7 @@
  // Registramos el service worker para PWA
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./sw.js", { scope: "./" })
+    .register("/app-huellas-tabatha/sw-v2.js", { scope: "./" })
     .then(() => console.log("SW registrado correctamente"))
     .catch(err => console.error("SW error:", err));
 }
